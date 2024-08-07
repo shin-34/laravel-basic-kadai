eb3f068c569db85afd8a390a6a28dac24254ce07
shin-34 <shinmegg@gmail.com>
2024-08-07 13:17:44 +0900

    Initial commit

---
+++ laravel-basic/app/Http/Controllers/Posts/PostController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Posts;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PostController extends Controller{
+    public function index(){
+        $posts = DB::table('posts')->get(); 
+        return view('index', compact('posts'));
+    }
+}
